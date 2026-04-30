f9f4cfd2670c416c64864e2eaf3ba389c3565c15
ChloeHsin <chsin1@babson.edu>
2026-04-30 10:52:56 -0400

    gunicorn

--- final project/start.sh
+++ final project/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-python -m pip install --upgrade pip
-python -m pip install --no-cache-dir gunicorn==20.1.0
-python -m gunicorn web_app:app --bind 0.0.0.0:$PORT
+#python -m pip install --upgrade pip
+#python -m pip install --no-cache-dir gunicorn==20.1.0
+#python -m gunicorn web_app:app --bind 0.0.0.0:$PORT
